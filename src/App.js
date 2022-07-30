fc1f0fc2171538787fe41af72ccd837608ab32ef
Nicolo De Rinaldis <nicoderin98@gmail.com>
2022-07-30 08:25:37 +0200

    finish AllInOne

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Support from "./components/Support";
 import AllInOne from "./components/AllInOne";
+import Pricing from "./components/Pricing";
 function App() {
   return (
     <>
@@ -11,6 +12,7 @@ function App() {
     <About />
     <Support />
     <AllInOne />
+    <Pricing />
     </>
   );
 }
--- src/components/AllInOne.jsx
+++ src/components/AllInOne.jsx
@@ -1,8 +1,161 @@
+import { CheckIcon } from '@heroicons/react/outline'
 import React from 'react'
 
 function AllInOne() {
   return (
-    <div>AllInOne</div>
+    <div className='w-full my-32 '>
+      <div className='max-w-[1240px] mx-auto pex-2'>
+        <h2 className='text-5xl font-bold text-center'>All-In-One Platform</h2>
+        <p className='text-2xl py-8 text-gray-500 text-center'>
+          It is a long established fact that a reader will be distracted
+          by the readable content of a page when looking at its layout.
+          The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters,
+          as opposed to using 'Content here, content here', making it look like readable English.
+        </p>
+
+      <div className='grid sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4'>
+          <div className='flex'>
+            <div>
+            <CheckIcon className='w-7 mr-4 text-green-600'/>
+            </div>
+
+            <div className=''>
+              <h3 className='font-bold text.lg'>Notifications</h3>
+              <p className='text-lg pt-2 pb-4'>
+                Many desktop publishing packages and web page editors
+                now use Lorem Ipsum as their default model text,
+                and a search for 'lorem ipsum' will uncover many web
+                sites still in their infancy. Various versions have evolved over the years,
+                sometimes by accident, sometimes on purpose
+                (injected humour and the like).
+                </p>
+            </div>
+          </div>
+          <div className='flex'>
+            <div>
+            <CheckIcon className='w-7 mr-4 text-green-600'/>
+            </div>
+
+            <div className=''>
+              <h3 className='font-bold text.lg'>Notifications</h3>
+              <p className='text-lg pt-2 pb-4'>
+                Many desktop publishing packages and web page editors
+                now use Lorem Ipsum as their default model text,
+                and a search for 'lorem ipsum' will uncover many web
+                sites still in their infancy. Various versions have evolved over the years,
+                sometimes by accident, sometimes on purpose
+                (injected humour and the like).
+                </p>
+            </div>
+          </div>
+          <div className='flex'>
+            <div>
+            <CheckIcon className='w-7 mr-4 text-green-600'/>
+            </div>
+
+            <div className=''>
+              <h3 className='font-bold text.lg'>Notifications</h3>
+              <p className='text-lg pt-2 pb-4'>
+                Many desktop publishing packages and web page editors
+                now use Lorem Ipsum as their default model text,
+                and a search for 'lorem ipsum' will uncover many web
+                sites still in their infancy. Various versions have evolved over the years,
+                sometimes by accident, sometimes on purpose
+                (injected humour and the like).
+                </p>
+            </div>
+          </div>
+          <div className='flex'>
+            <div>
+            <CheckIcon className='w-7 mr-4 text-green-600'/>
+            </div>
+
+            <div className=''>
+              <h3 className='font-bold text.lg'>Notifications</h3>
+              <p className='text-lg pt-2 pb-4'>
+                Many desktop publishing packages and web page editors
+                now use Lorem Ipsum as their default model text,
+                and a search for 'lorem ipsum' will uncover many web
+                sites still in their infancy. Various versions have evolved over the years,
+                sometimes by accident, sometimes on purpose
+                (injected humour and the like).
+                </p>
+            </div>
+          </div>
+          <div className='flex'>
+            <div>
+            <CheckIcon className='w-7 mr-4 text-green-600'/>
+            </div>
+
+            <div className=''>
+              <h3 className='font-bold text.lg'>Notifications</h3>
+              <p className='text-lg pt-2 pb-4'>
+                Many desktop publishing packages and web page editors
+                now use Lorem Ipsum as their default model text,
+                and a search for 'lorem ipsum' will uncover many web
+                sites still in their infancy. Various versions have evolved over the years,
+                sometimes by accident, sometimes on purpose
+                (injected humour and the like).
+                </p>
+            </div>
+          </div>
+          <div className='flex'>
+            <div>
+            <CheckIcon className='w-7 mr-4 text-green-600'/>
+            </div>
+
+            <div className=''>
+              <h3 className='font-bold text.lg'>Notifications</h3>
+              <p className='text-lg pt-2 pb-4'>
+                Many desktop publishing packages and web page editors
+                now use Lorem Ipsum as their default model text,
+                and a search for 'lorem ipsum' will uncover many web
+                sites still in their infancy. Various versions have evolved over the years,
+                sometimes by accident, sometimes on purpose
+                (injected humour and the like).
+                </p>
+            </div>
+          </div>
+          <div className='flex'>
+            <div>
+            <CheckIcon className='w-7 mr-4 text-green-600'/>
+            </div>
+
+            <div className=''>
+              <h3 className='font-bold text.lg'>Notifications</h3>
+              <p className='text-lg pt-2 pb-4'>
+                Many desktop publishing packages and web page editors
+                now use Lorem Ipsum as their default model text,
+                and a search for 'lorem ipsum' will uncover many web
+                sites still in their infancy. Various versions have evolved over the years,
+                sometimes by accident, sometimes on purpose
+                (injected humour and the like).
+                </p>
+            </div>
+          </div>
+          <div className='flex'>
+            <div>
+            <CheckIcon className='w-7 mr-4 text-green-600'/>
+            </div>
+
+            <div className=''>
+              <h3 className='font-bold text.lg'>Notifications</h3>
+              <p className='text-lg pt-2 pb-4'>
+                Many desktop publishing packages and web page editors
+                now use Lorem Ipsum as their default model text,
+                and a search for 'lorem ipsum' will uncover many web
+                sites still in their infancy. Various versions have evolved over the years,
+                sometimes by accident, sometimes on purpose
+                (injected humour and the like).
+                </p>
+            </div>
+          </div>
+      </div>
+
+      </div>
+
+
+    </div>
   )
 }
 
