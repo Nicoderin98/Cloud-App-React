6f7439f62e8f30b43c46a80a615ac35f3d974666
Nicolo De Rinaldis <nicoderin98@gmail.com>
2022-07-30 08:27:15 +0200

    fixed text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -26,7 +26,7 @@ function Hero() {
         border border-slate-300 rounded-xl text-center shadow-xl'>
           <p>Data Services</p>
           <div className='flex justify-between flex-wrap px-4'>
-            <p className='flex px-4 py-2 text-slate-500'><CloudUploadIcon className='h-6 text-indigo-600'/>. App Security</p>
+            <p className='flex px-4 py-2 text-slate-500'><CloudUploadIcon className='h-6 text-indigo-600'/>App Security</p>
             <p className='flex px-4 py-2 text-slate-500'><DatabaseIcon className='h-6 text-indigo-600'/> Dashboard Design</p>
             <p className='flex px-4 py-2 text-slate-500'><ServerIcon className='h-6 text-indigo-600'/> Cloud Data</p>
             <p className='flex px-4 py-2 text-slate-500'><PaperAirplaneIcon className='h-6 text-indigo-600'/> API</p>
